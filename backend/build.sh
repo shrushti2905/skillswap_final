44fae50d6c2ebf539cdc37f32e33681adecf6287
shrushti2905 <shrushtipatil2905@gmail.com>
2026-04-08 19:07:00 +0530

    Optimize build script - remove seed_database from deploy

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python seed_database.py
